c0625c7ec57f0c4cb3bc32a0fd63c1461b9da44f
rmrokon <mdrokon7773@gmail.com>
2022-02-17 02:50:46 +0600

    Save button click handler done

--- JS/world-bank.js
+++ JS/world-bank.js
@@ -1,8 +1,15 @@
 // Not a number value to Number conversion function
 function convertIntoNum(givenId) {
     const NaNvalue = document.getElementById(givenId);
-    const valueInNum = parseFloat(NaNvalue.value);
-    return valueInNum;
+    if (givenId.includes('display')) {
+        const valueInNum = parseFloat(NaNvalue.innerText);
+        return valueInNum;
+    }
+
+    else {
+        const valueInNum = parseFloat(NaNvalue.value);
+        return valueInNum;
+    }
 }
 
 // calculating total expense
@@ -15,15 +22,39 @@ function calcTotalExpense() {
     return totalExpense;
 }
 
-document.getElementById('calculate-btn').addEventListener('click', function () {
+//  Calculate Balance
+
+function calcBalance() {
     const income = convertIntoNum('income');
     const totalExpense = calcTotalExpense();
-
-    // Displaying total expense
-    document.getElementById('display-total-expense').innerText = totalExpense;
-
     const balance = income - totalExpense;
+    document.getElementById('display-total-expense').innerText = totalExpense;
+    return balance;
+}
 
+// Calculate Saving
+
+function calcSaving() {
+    const givenSavingPercentage = convertIntoNum('savings');
+    const currentBalance = calcBalance();
+    const savings = currentBalance * (givenSavingPercentage / 100);
+    return savings;
+}
+
+
+// Calculate Button Event Handler
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const balance = calcBalance();
     // Displaying total balance
     document.getElementById('display-total-balance').innerText = balance;
+})
+
+// Saving Button Event Handler
+document.getElementById('savings-btn').addEventListener('click', function () {
+    const savingAmount = calcSaving();
+    const currentBalance = calcBalance();
+    const remainingBalance = currentBalance - savingAmount;
+    document.getElementById('display-total-savings').innerText = savingAmount;
+    document.getElementById('display-total-remaining-balance').innerText = remainingBalance;
 })
